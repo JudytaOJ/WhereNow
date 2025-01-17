da911c357275d33cbaf55a116d44da4c0f773c55
„Judyta” <judyta.ostapiak@gmail.com>
2025-01-17 22:53:14 +0100

    Upgrade of dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.example.wherenow"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.wherenow"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
